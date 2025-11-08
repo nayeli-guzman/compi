a02500d351a4c920754eb96aa4969eb44e214230
ReatorG <renato.garciacalle12@gmail.com>
2025-11-08 11:43:35 -0500

    add: cont6

---
+++ continua-7/visitor.h
@@ -0,0 +1,66 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+#include "ast.h"
+#include <list>
+
+class BinaryExp;
+class NumberExp;
+class IdExp;
+class AssignStm;
+class PrintStm;
+class IfStm;
+class WhileStm;
+class Program;
+
+class Visitor {
+public:
+    virtual int visit(BinaryExp* exp) = 0;
+    virtual int visit(NumberExp* exp) = 0;
+    virtual int visit(IdExp* exp) = 0;
+    virtual void visit(AssignStm* stm) = 0;
+    virtual void visit(IfStm* stm) = 0;
+    virtual void visit(WhileStm* stm) = 0;
+    virtual void visit(PrintStm* stm) = 0;
+};
+
+class GencodeVisitor : public Visitor {
+public:
+    unordered_map<string, int> env;
+    int contador = 1;
+    void code(Program* program);
+    int visit(BinaryExp* exp) override;
+    int visit(NumberExp* exp) override;
+    int visit(IdExp* exp) override;
+    void visit(AssignStm* stm) override;
+    void visit(IfStm* stm) override;
+        void visit(WhileStm* stm) override;
+    void visit(PrintStm* stm) override;
+};
+
+class PrintVisitor : public Visitor {
+public:
+    void imprimir(Program* program);
+    int visit(BinaryExp* exp) override;
+    int visit(NumberExp* exp) override;
+    int visit(IdExp* exp) override;
+        void visit(WhileStm* stm) override;
+    void visit(AssignStm* stm) override;
+    void visit(PrintStm* stm) override;
+   void visit(IfStm* stm) override;
+
+};
+
+class EVALVisitor : public Visitor {
+public:
+    void interprete(Program* program);
+    int visit(BinaryExp* exp) override;
+    int visit(NumberExp* exp) override;
+    int visit(IdExp* exp) override;
+        void visit(WhileStm* stm) override;
+    void visit(AssignStm* stm) override;
+    void visit(PrintStm* stm) override;
+   void visit(IfStm* stm) override;
+
+};
+
+#endif // VISITOR_H
